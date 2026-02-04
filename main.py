84f663d121b61b57186a5e8b58bffc6d2c979409
Your Name <you@example.com>
2026-02-04 22:04:11 +0100

    garage2

--- main.py
+++ main.py
@@ -25,11 +25,16 @@ def on_startup():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200", "http://127.0.0.1:4200"],
+    allow_origins=[
+        "http://localhost:4200",
+        "http://127.0.0.1:4200",
+        "https://garage-frontend-mu.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 def utcnow():
